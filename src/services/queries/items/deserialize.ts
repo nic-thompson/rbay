6a032953fbddd74850c6289f3ecb9160d1440268
Nicolas Thompson <mail@nicolasthompson.com>
2024-05-18 15:06:25 +0100

    Gets an item

--- src/services/queries/items/deserialize.ts
+++ src/services/queries/items/deserialize.ts
@@ -1,4 +1,19 @@
 import type { Item } from '$services/types';
 import { DateTime } from 'luxon';
 
-export const deserialize = (id: string, item: { [key: string]: string }): Item => {};
+export const deserialize = (id: string, item: { [key: string]: string }): Item => {
+	return {
+		id,
+		name: item.name,
+		description: item.description,
+		createdAt: DateTime.fromMillis(parseInt(item.createdAt)),
+		endingAt: DateTime.fromMillis(parseInt(item.endingAt)),
+		imageUrl: item.imageUrl,
+		highestBidUserId: item.highestBidUserId,
+		ownerId: item.ownerId,
+		views: parseInt(item.views),
+		likes: parseInt(item.likes),
+		bids: parseInt(item.bids),
+		price: parseInt(item.price)
+	};
+};
